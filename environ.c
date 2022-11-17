ffb0263925c480db03b493e94f86fef91ca2e606
ichebadu <101195914+ichebadu@users.noreply.github.com>
2022-11-17 18:01:23 +0100

    Update environ.c
    prints the current environment

--- environ.c
+++ environ.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- * _myenv - prints the current environment
+ * _myenv - This prints the current environment
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
