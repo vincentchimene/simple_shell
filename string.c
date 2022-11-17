bdf37db3a7336230451be162c2bb6812fe9845cc
ichebadu <101195914+ichebadu@users.noreply.github.com>
2022-11-17 18:53:15 +0100

    Update string.c
    strings manipulations

--- string.c
+++ string.c
@@ -1,6 +1,6 @@
 #include "shell.h"
 /**
- * _strlen - returns the length of a string
+ * _strlen - it returns the length of a string
  * @s: the string whose length to check
  * Return: integer length of string
  */
