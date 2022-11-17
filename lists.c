584ce5c19b9bf18a4b14f4365c45468493f4d6e3
ichebadu <101195914+ichebadu@users.noreply.github.com>
2022-11-17 18:30:33 +0100

    Update lists.c
    listing

--- lists.c
+++ lists.c
@@ -1,8 +1,8 @@
 #include "shell.h"
 
 /**
- * add_node - adds a node to the start of the list
- * @head: address of pointer to head node
+ * add_node - Adds a node to the start of the list
+ * @head: Address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
  *
